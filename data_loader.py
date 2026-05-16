e703a0ab93dd71e900d67fbccf467281c4460d58
Wodiin <samphillips246@gmail.com>
2026-05-17 01:25:38 +1000

    Restructured and fixed spelling mistakes in file names. Added data loader to read JSON files.

---
+++ data_loader.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+# Loads all the data from the data folder and the kits folder and returns it as a dictionary
+
+def load_data():
+    data = {}
+    folders = ['data','./data/kits']
+
+    # Loop through each folder and load the JSON files into the data dictionary
+    for folder in folders:
+        for file in os.listdir(folder):
+            if file.endswith('.json'):
+                with open(os.path.join(folder, file), 'r') as f:
+                    data[file.removesuffix(".json")] = json.load(f)
+    return data
